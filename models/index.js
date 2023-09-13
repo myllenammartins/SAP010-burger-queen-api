55110669148c125f4edecb48733bfa7318b3722f
unknown <mirandamartins05@gmail.com>
2023-09-13 11:06:04 -0300

    alteração nova na pasta models, index.js

--- models/index.js
+++ models/index.js
@@ -2,21 +2,18 @@ const fs = require('fs');
 const path = require('path');
 const process = require('process');
 const Sequelize = require('sequelize');
-
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const configPath = path.join(__dirname, '..', 'config', 'config.json');
 // eslint-disable-next-line import/no-dynamic-require
 const config = require(configPath)[env];
 const db = {};
-
 let sequelize;
-if (config.DATABASE_URL) {
-  sequelize = new Sequelize(process.env[config.DATABASE_URL], config);
+if (config.use_env_variable === 'DATABASE_URL') {
+  sequelize = new Sequelize(process.env.DATABASE_URL, config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
-
 fs
   .readdirSync(__dirname)
   .filter((file) => (
@@ -27,14 +24,11 @@ fs
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
-
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
-
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-
 module.exports = db;
