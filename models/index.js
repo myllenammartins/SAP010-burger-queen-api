8852e9577844a1deb12318eec29cdf689cdda442
unknown <mirandamartins05@gmail.com>
2023-09-13 10:54:03 -0300

    alteração na pasta models, index.js

--- models/index.js
+++ models/index.js
@@ -11,8 +11,8 @@ const config = require(configPath)[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (config.DATABASE_URL) {
+  sequelize = new Sequelize(process.env[config.DATABASE_URL], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
